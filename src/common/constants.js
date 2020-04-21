28ee52db3331c3a4689698691eb75c2ca81a0c4f
mukundwagh <mukundwagh@gmail.com>
2020-04-21 17:47:42 -0400

    updated backend url

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://zomato-backend-server.herokuapp.com/api";
+export const API_URL = "https://webdev-sp20-backend.herokuapp.com/api";
 export const ZOMATO_API_URL = "https://developers.zomato.com/api/v2.1";
 export const USER_KEY = "77b837dcd6ed20dfed2ef5ebaa837c05";
